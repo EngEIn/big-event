c833a9b94d20bc30fe436ab5dfd2e6f71569db9f
engin <egeini@163.com>
2024-07-14 14:57:57 +0800

    first commit

---
+++ src/main/java/com/back/bigEventApplication.java
@@ -0,0 +1,13 @@
+package com.back;
+
+/**
+ * Hello world!
+ *
+ */
+public class bigEventApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
